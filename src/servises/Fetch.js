10d4e31bb10130b7643d1f0c1ce7e70a0a3c8425
Anton Kutsim <kutsimanton@gmail.com>
2023-02-26 15:19:21 +0200

    f

--- src/servises/Fetch.js
+++ src/servises/Fetch.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?'
 
 export const baseFetch = (location) => {
-    return axios.get(`${BASE_URL}q=${location}&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
+    return axios.get(`${BASE_URL}q=${location}&appid=90792bd8e1024cd9cf8bda710105a342&units=metric`)
 }
